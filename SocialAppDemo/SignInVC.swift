8c56a644b84d94f750b01b82f0aec45cff7e5a83
Andrienne Liew <andrienneliew@Andriennes-MacBook-Air.local>
2016-12-30 14:35:08 +0800

    Completed setting up Firebase integration

---
+++ SocialAppDemo/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SigninVC.swift
+//  SocialAppDemo
+//
+//  Created by Andrienne Liew on 30/12/2016.
+//  Copyright © 2016 AppDeck. All rights reserved.
+//
+
+import UIKit
+
+class SigninVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
