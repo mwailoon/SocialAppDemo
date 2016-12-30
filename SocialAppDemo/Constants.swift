398cf8373c552166ca136b383bf0d9657902c6fb
Andrienne Liew <andrienneliew@Andriennes-MacBook-Air.local>
2016-12-30 16:36:24 +0800

    Sign in ui custom classes completed

---
+++ SocialAppDemo/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialAppDemo
+//
+//  Created by Andrienne Liew on 30/12/2016.
+//  Copyright © 2016 AppDeck. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
