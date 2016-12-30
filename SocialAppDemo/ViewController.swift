d0cb760889aa11afdd47ae93cd0c1857c55e578e
Andrienne Liew <andrienneliew@andriennes-macbook-air.local>
2016-12-30 14:18:01 +0800

    Initial Commit

---
+++ SocialAppDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialAppDemo
+//
+//  Created by Andrienne Liew on 30/12/2016.
+//  Copyright © 2016 AppDeck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
